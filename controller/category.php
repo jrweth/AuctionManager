2882d4fdba9e88b9df18192c38fd1e48b13e47d1
J. Reuben Wetherbee <jrweth@gmail.com>
2013-07-01 01:23:13 -0400

    quick first version

---
+++ controller/category.php
@@ -0,0 +1,24 @@
+<?php 
+
+$dataTypeColumns = array('id', 'name');
+            
+
+switch ($_REQUEST['action']) {
+    
+    case 'list':
+    default:
+        $sql = 'select ' .implode(', ' , $dataTypeColumns) .' from Category';
+        
+        $result1 = $db->query($sql);
+        if(!$result1) {
+            $redtextmsg =  "query1 of $thiscategory table failed";
+            print 'err';
+        } else {
+        
+            $data = array();
+            while($res = $result1->fetchArray(SQLITE3_ASSOC)){
+                $data[] = $res;
+            }
+        } 
+        echo $twig->render('category/list.html.twig', array('data' => $data));
+}
---
+++ web/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+require_once '../vendor/autoload.php';
+$config = parse_ini_file('config/config.ini');
+
+
+
+$loader = new Twig_Loader_Filesystem($config['projectRoot'].'template');
+$twig = new Twig_Environment($loader);
+
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('../db/auction.db');
+    }
+}
+
+$db = new MyDB();
+
+switch ($_REQUEST['dataType']) {
+
+    case 'category': include $config['projectRoot'] . 'controller/category.php'; break;
+    default:
+        echo $twig->render('layout.html.twig', array('name' => 'Fabien'));
+}
+
+
+
+?>
