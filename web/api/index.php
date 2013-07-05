3a40612a14e7128210a323ae86e8793f9f0b017d
J. Reuben Wetherbee <jrweth@gmail.com>
2013-07-05 12:01:52 -0400

    removed indent and also made return from insert and update return id and values

--- web/api/index.php
+++ web/api/index.php
@@ -22,7 +22,7 @@ Slim\Slim::registerAutoloader();
 
 //set up the slim app and the 
 $app = new Slim\Slim();
-//$app->contentType('application/json');
+$app->contentType('application/json');
 
 //set up the db
 $db = new PDO('sqlite:' . $config['projectRoot'].$config['dbPath']);
@@ -37,100 +37,48 @@ function outputResult($action, $success = true, $id = 0)
 }
 
 $app->get('/', function () use($db) {
-    echo '<html><head>
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript">
-submitUpdate = function(tableName) {
-    var formVars = $("#updateForm").serialize();
-    $.ajax({
-       url: tableName + "/2",
-       type: "PUT",
-       data: formVars,
-       dataType: "json",
-       error: function(jqXHR, textStatus, errorThrow){
-         alert("error");
-         },
-        success: function(data){
-         alert(data);
-       },
-      });
-      }
-submitDelete = function(tableName) {
-    var formVars = $("#updateForm").serialize();
-    $.ajax({
-       url: tableName + "/2",
-       type: "DELETE",
-       dataType: "json",
-       error: function(jqXHR, textStatus, errorThrow){
-         alert("error");
-         },
-        success: function(data){
-         alert(data.success);
-       },
-      });
-      }
-  </script>
-</head>
-<body>
-    <h1>category</h1>
-    <form action="category" method="post">
-    <input name="name" />
-    <input type="submit" />
-    <input type="hidden" name="auction_group_id" value="1" />
-    </form>
-    update<form id="updateForm" action="category/6" method="put">
-    <input name="name" /><a onclick="submitUpdate(\'category\')">update</a>
-    </form>
-    <button onclick="submitDelete(\'category\')">delete</button>
-    <h1>auction group</h1>
-    <form action="auction_group" method="post">
-    <input name="name" />
-    <input type="submit" />
-    </form>
-    update<form id="updateForm" action="auction_group/1" method="put">
-    <input name="name" /><a onclick="submitUpdate()">update</a>
-    </form>
-    <button onclick="submitDelete(\'auction_group\')">delete</button>
-</body></html>';
+    echo '';
 });
 
 $app->get('/:tableName', function ($tableName) use ($db) {
     echo json_encode(DBHelper::getTableRecords($db, $tableName));
 });
 
-$app->get('/:tableName/:id', function ($tableName, $id) use ($db) {
+$app->get('/:tableName/:id', function ($tableName, $id) use ($db, $app) {
     if($record = DBHelper::getTableRecord($db, $tableName, $id)) {
         echo json_encode($record);
     }
     else {
-        echo 'error';
+        $app->halt(500, 'An Error Occured trying to retrieve the records.');
     }
 });
 
 $app->post('/:tableName', function ($tableName) use ($db, $app) {
-    if($id = DBHelper::insertTableRecord($db, $tableName, $app->request()->post())) {
-        outputResult('add', true, $id);
+    $values = json_decode($app->request()->getBody(), true);
+    if($id = DBHelper::insertTableRecord($db, $tableName, $values)) {
+        $values['id'] = $id;
+        echo json_encode($values);
     }
     else {
-        outputResult('add', false, null);
+        $app->halt(500, 'An Error Occured trying to save the new record.');
     }
 });
 
 
 $app->put('/:tableName/:id', function ($tableName, $id) use ($db, $app) {
-    outputResult(
-       'add',
-       DBHelper::updateTableRecord($db, $tableName, $id, $app->request()->post()),
-       $id
-    );
+    $values = json_decode($app->request()->getBody(), true);
+    if(DBHelper::updateTableRecord($db, $tableName, $id, $values)) {
+        echo json_encode($values);
+    }
+    else {
+        $app->halt(500, 'An Error Occured trying to save the record.');
+    }
 });
 
 $app->delete('/:tableName/:id', function ($tableName, $id) use ($db) {
-    outputResult(
-       'delete',
-       DBHelper::deleteTableRecord($db, $tableName, $id),
-       $id
-    );
+    if(!DBHelper::deleteTableRecord($db, $tableName, $id)) {
+        $app->halt(500, 'An Error Occured trying to delete the record.');
+    }
 });
 
 
