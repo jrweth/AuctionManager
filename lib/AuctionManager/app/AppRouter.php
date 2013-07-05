3c34597168f4ca2cf9b515fdf2732055bcb18929
J. Reuben Wetherbee <jrweth@gmail.com>
2013-07-05 12:04:56 -0400

    added the app router

---
+++ lib/AuctionManager/app/AppRouter.php
@@ -0,0 +1,38 @@
+<?php 
+namespace AuctionManager\app;
+
+class AppRouter
+{
+    /** @var array */
+    protected $container;
+    
+    /** @var \Slim\Slim */
+    protected $app;
+    
+    /**
+     * Set up Router 
+     * @param arary $container
+     */
+    public function __construct($container)
+    {
+        $this->container = $container;
+        $this->app = $container['app'];
+    }
+    
+    //define all of the routes 
+    public function route() {
+        $container = $this->container;
+        
+        $this->app->get('/', function () use($container) {
+            echo $container['twig']->render('layout.html.twig');
+        });
+        
+        $this->app->get('/category', function() use($container) {
+        
+            $controller = new \AuctionManager\controller\Category($container);
+            echo $controller->listAction();
+        });
+    }
+}
+
+?>
